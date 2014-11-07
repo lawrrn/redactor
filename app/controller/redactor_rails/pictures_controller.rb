9dd79d6568771bde03bf684fdb1062508b998b70
lawrrn <lawrr17@gmail.com>
2014-11-07 12:48:33 -0800

    Update pictures_controller.rb

--- app/controller/redactor_rails/pictures_controller.rb
+++ app/controller/redactor_rails/pictures_controller.rb
@@ -1,34 +1,34 @@
 class RedactorRails::PicturesController < ApplicationController
-  before_filter :redactor_authenticate_user!
 
   def index
-    @pictures = RedactorRails.picture_model.where(
-        RedactorRails.picture_model.new.respond_to?(RedactorRails.devise_user) ? { RedactorRails.devise_user_key => redactor_current_user.id } : { })
-    render :json => @pictures.to_json
+    @redactors = controller_model.order :id
+    render json: @redactors.to_json
   end
 
   def create
-    @picture = RedactorRails.picture_model.new
+    @redactor = controller_model.new
 
     file = params[:file]
-    @picture.data = RedactorRails::Http.normalize_param(file, request)
-    if @picture.has_attribute?(:"#{RedactorRails.devise_user_key}")
-      @picture.send("#{RedactorRails.devise_user}=", redactor_current_user)
-      @picture.assetable = redactor_current_user
+    @redactor.data = RedactorRails::Http.normalize_param(file, request)
+    if current_user.present?
+      @redactor.user_id = current_user.id
     end
 
-    if @picture.save
-      render :text => { :filelink => @picture.url }.to_json
+    if @redactor.save
+      render text: { filelink: @redactor.url }.to_json
     else
-      render json: { error: @picture.errors }
+      render nothing: true
     end
   end
 
-  private
+  protected
 
-  def redactor_authenticate_user!
-    if RedactorRails.picture_model.new.has_attribute?(RedactorRails.devise_user)
-      super
-    end
+  def controller_model
+    model_name(self.class.to_s.sub('Controller', '').singularize).constantize
   end
+
+  def model_name(name)
+    name == "RedactorRails::Document" ? "RedactorRails::Attachment" : name
+  end
+
 end
